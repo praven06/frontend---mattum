acf5450684827077a92c89a4cd4b0998dc6654f9
praveen kumar <107418868+praven06@users.noreply.github.com>
2025-02-07 15:56:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import {  Home } from "./components/Home"
-
+import { BrowserRouter,  Route, Routes } from "react-router-dom"
+import Login from "./components/Login"
 function App() {
  
 
   return (
-    <>
-      <Home/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
